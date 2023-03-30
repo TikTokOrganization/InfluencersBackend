db0906d57b0e4485d7b4fd5e245c0edb97f51259
Zach Jones <jones.6606@osu.edu>
2023-03-29 21:45:07 -0400

    Changed port being used for elastic (idk why)

--- youtubeShortsGetter.py
+++ youtubeShortsGetter.py
@@ -64,7 +64,7 @@ class youtubeShortsGetter:
 
     def initializeElasticIndex(self) -> None:
         #connects to elasticsearch
-        es = Elasticsearch('http://localhost:9200')
+        es = Elasticsearch('http://localhost:9201')
 
         #deletes index if it exists and loads json file with all of the liked videos
         if es.indices.exists(index="myindex"):
